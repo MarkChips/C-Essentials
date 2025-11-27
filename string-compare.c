449143285630e2481750ffc33ed1bdb31857e98e
MarkChips <mark-chipperfield@outlook.com>
2025-11-27 21:39:21 +0000

    demo string comparison using strcmp method

--- string-compare.c
+++ string-compare.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 void main (void)
 {
 	char str1[10] = "first";
 	char str2[10] = "fire";
-	char *ptr1 = str1, *ptr2 = str2;
-	
-	while (*ptr1 != 0 && *ptr2 != 0)
-	{
-		if (*ptr1 != *ptr2)
-		{
-			break;
-		}
-		ptr1++;
-		ptr2++;
-	}
-	
-	if (*ptr1 == 0 && *ptr2 == 0)
+	if (strcmp (str1, str2) == 0)
 	{
 		printf ("The two strings are identical.\n");
 	}
