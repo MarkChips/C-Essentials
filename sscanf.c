5dfdb6b03c56cc487d0a629162fed6b34b38f3f4
MarkChips <mark-chipperfield@outlook.com>
2025-11-27 22:07:59 +0000

    read multiple format specifiers using sscanf

--- sscanf.c
+++ sscanf.c
@@ -3,8 +3,15 @@
 void main (void)
 {
 	int val;
-	char string[10] = "250";
+	char result[10];
+	char string[25] = "The first number is 1";
 	
-	sscanf (string, "%d", &val);
-	printf ("The value in the string is %d\n", val);
+	if (sscanf (string, "The %s number is %d", result, &val) == 2)
+	{
+		printf ("String : %s Value : %d\n", result, val);
+	}
+	else
+	{
+		printf ("I couldn't find two values in that string.\n");
+	}
 }
