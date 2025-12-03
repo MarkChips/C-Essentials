e1618c0e408501ce781dc8ad501e5d7ac7302b70
MarkChips <mark-chipperfield@outlook.com>
2025-12-03 17:00:42 +0000

    demo #define functions

--- chapter12/preprocessor.c
+++ chapter12/preprocessor.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#define PI 3.14159
+#define ADD(a, b) (a + b)
 
 void main (void)
 {
-	float rad = 3;
-	float circ = rad * 2 * PI;
-	float area = rad * rad * PI;
-	printf ("The circumference of a circle radius %f is %f\n", rad, circ);
-	printf ("The area of a circle radius %f is %f\n", rad, area);
+	printf ("The sum of %d and %d is %d\n", 5, 2, ADD(5, 2));
+	printf ("The sum of %d and %d is %d\n", 3, 7, ADD(3, 7));
 }
